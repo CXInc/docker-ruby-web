ee9d43f790e3363663f10c2582ef11c9d53f1d36
Bruz Marzolf <bruz@bruzilla.com>
2015-01-13 13:41:40 -0800

    Use unicorn preloading to reduce memory usage

--- unicorn.rb
+++ unicorn.rb
@@ -13,3 +13,24 @@ listen "#{@dir}tmp/sockets/unicorn.sock", :backlog => 64
 
 # Set process id path
 pid "#{@dir}tmp/pids/unicorn.pid"
+
+preload_app true
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
